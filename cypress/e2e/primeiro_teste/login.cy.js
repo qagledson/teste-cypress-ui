464bb3802c62773a3cdb8f3824e1bdd26c1fd094
Gledson Nascimento <gledsonnascimento17@gmail.com>
2023-10-06 17:58:27 -0300

    usando massa de dados

--- cypress/e2e/primeiro_teste/login.cy.js
+++ cypress/e2e/primeiro_teste/login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 context('Funcionaldade de login', () => {
     //Hooks beforeEach - é executado antes de cada teste
@@ -17,6 +18,25 @@ context('Funcionaldade de login', () => {
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it('Deve efetuar login com sucesso usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it.only('Deve efetuar login com sucesso usando fixtures', () => {
+        cy.fixture('perfil.json').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+    
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
+
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
