0ce76da5c220bea3be932fee8c3ca5bdae9578b2
Gledson Nascimento <gledsonnascimento17@gmail.com>
2023-10-06 16:55:24 -0300

    alterações no teste automatizado

--- cypress/e2e/primeiro_teste/login.cy.js
+++ cypress/e2e/primeiro_teste/login.cy.js
@@ -1,9 +1,17 @@
 /// <reference types="cypress"/>
 
 context('Funcionaldade de login', () => {
+    //Hooks beforeEach - é executado antes de cada teste
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    //Hooks beforeEach - é executado antes de cada teste
+    afterEach(() => {
+        cy.screenshot()
+    });
 
     it('Deve efetuar login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -12,7 +20,6 @@ context('Funcionaldade de login', () => {
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('alu_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -22,13 +29,12 @@ context('Funcionaldade de login', () => {
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('te@teste.com')
         cy.get('.woocommerce-form > .button').click()
         
 
-        
+
         cy.get('.woocommerce-error').should('contain', 'a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta')
     })
 })
---
+++ cypress/e2e/primeiro_teste/produtos.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress"/>
+//var faker = require('faker')
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade Página de Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+        //- clica no primeiro item da lista
+        //.first() 
+        //- clica no último item da lista
+        //.last() 
+        //- clica no item da lista conforme a posição informada
+        //.eq(3)
+        .contains('Ariel Roll Sleeve Sweatshirt')
+        .click()
+        
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 10
+        //- Selecionar produto
+        cy.get('[class="product-block grid"]')
+        .contains('Abominable Hoodie')
+        .click()
+        //- Selecionar tamanho
+        cy.get('ul li[data-value="XS"]').click()
+         //- Selecionar cor
+        cy.get('ul li[data-value="Blue"]').click()
+         //- Selecionar quantidade
+        cy.get('input[name="quantity"]').clear().type(quantidade)
+        //- Selecionar quantidade
+        cy.get('.single_add_to_cart_button').click()
+
+        //validações
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade +' × “Abominable Hoodie” foram adicionados no seu carrinho')
+    });
+
+});
