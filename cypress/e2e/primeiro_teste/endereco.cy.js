29420d591504e0c6535374be22e09959815176dd
Gledson Nascimento <gledsonnascimento17@gmail.com>
2023-10-07 16:02:04 -0300

    Adicionando massa de dados de cadastro de endereço

--- cypress/e2e/primeiro_teste/endereco.cy.js
+++ cypress/e2e/primeiro_teste/endereco.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
-//var faker = require('faker')
-import { faker } from '@faker-js/faker';
+import enderecoPage from '../../support/page-objetc/enderecoPage';
+const dadosEndereco = require('../../fixtures/endereco.json')
+
 
 describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
     
@@ -9,12 +10,23 @@ describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
         cy.fixture('perfil.json').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
-
     });
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        //nessa etapa, ao adicionar a const "dadosEndereco", é possível acessar a lista de dados "[]" com seus parâmetros
+        const posicao_array = 0;
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[posicao_array].nome,
+            dadosEndereco[posicao_array].sobrenome,
+            dadosEndereco[posicao_array].empresa,
+            dadosEndereco[posicao_array].pais,
+            dadosEndereco[posicao_array].endereco,
+            dadosEndereco[posicao_array].numero,
+            dadosEndereco[posicao_array].cidade,
+            dadosEndereco[posicao_array].estado,
+            dadosEndereco[posicao_array].cep,
+            dadosEndereco[posicao_array].telefone,
+            dadosEndereco[posicao_array].email)
         
     });
 });
